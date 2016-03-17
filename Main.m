ba6f8e890df5d81216cbd6b53ae9113c4a1acbed
JWCS <sauve031@umn.edu>
2016-03-16 22:34:37 -0700

    Expansion in 8-connectivity complete. Cleaned up EdgeOperator

--- Main.m
+++ Main.m
@@ -6,10 +6,10 @@ Im = rot90( imread('H1.JPG'), -1 ); %Nature of the im, it's rotated (to me)
 %Im2 = Filter(Im); %Will implement filter later, partly done by threshold
 [ImMag, ImDir] = EdgeOperator( Im, 'sobel3' ); %Implement "names", use Im2
 %It is type specific, the output should be ImMag, not Im3
-ImMag( ImMag < threshold ) = 0;
+ImMag( ImMag < threshold ) = 0; ImDir( ImMag < threshold ) = 0;
 toc
-%Im3 = Expansion( Im3 );
-image( ImMag )
+[newMags, newDirs] = Expansion( ImMag, ImDir );
+image( newMags )
 %fancy plotting line following/finding stuff (Goal: begin no later than
 %Wednesday)
 toc
